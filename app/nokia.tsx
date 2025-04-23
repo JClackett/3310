7ea6a1cd3765f74180d3ffa96b56c8c6d969cc95
Jack Clackett <jack@noquarter.co>
2025-04-23 17:01:08 +0200

    bigger!

--- app/nokia.tsx
+++ app/nokia.tsx
@@ -185,178 +185,281 @@ export default function Nokia3310Simulator() {
 	}, []);
 
 	return (
-		<div className="z-0 bg-[#EFEFEF] select-none h-dvh w-screen overflow-hidden flex flex-col items-center pt-10 p-4 touch-none">
-			<div
-				className={cn(
-					"relative w-[400px] h-[580px] transition-opacity duration-300",
-					isReady ? "opacity-100" : "opacity-0",
-				)}
-			>
-				{/* Nokia 3310 Image */}
-				<div className="relative w-full h-full">
-					<Image
-						src="/nokia-3310-bg.png"
-						alt="Nokia 3310"
-						fill
-						onLoad={() => setIsReady(true)}
-						className="z-6 object-contain touch-none"
-						priority
-						loading="eager"
-						sizes="400px"
-					/>
+		<div
+			className={cn(
+				"z-0 bg-[#EFEFEF] h-dvh py-[72px] font-mono transition-opacity duration-300 relative select-none overflow-hidden flex flex-col items-center touch-none",
+				isReady ? "opacity-100" : "opacity-0",
+			)}
+		>
+			<div className="relative h-full">
+				<Image
+					src="/3310.png"
+					alt="Nokia 3310"
+					onLoad={() => setIsReady(true)}
+					className="!z-[6] relative object-contain touch-none h-full w-[600px]"
+					priority
+					loading="eager"
+					width={600}
+					height={1350}
+				/>
 
-					{/* Screen Overlay */}
+				{/* Screen Overlay */}
+				<div
+					style={{
+						left: 195,
+						top: 191,
+						width: 207,
+						height: 148,
+					}}
+					className="absolute z-5 shadow-[inset_0px_4px_10px_1px_rgb(0_0_0_/_0.8)] rounded-t-sm rounded-b-[12px] bg-gradient-to-br from-[#55684E] to-[#55684E]/70 flex flex-col items-center justify-center p-3 pb-3 overflow-scroll text-black text-sm"
+				>
+					<div className="absolute inset-0 grid grid-cols-[repeat(47,1fr)] grid-rows-[repeat(33,1fr)] pointer-events-none">
+						{[...Array(47 * 33)].map((_, i) => (
+							<div
+								key={`pixel-${Math.floor(i / 47)}-${i % 47}`}
+								className="border-[0.5px] !border-gray-700/5"
+							/>
+						))}
+					</div>
 
-					<div className="absolute z-5 shadow-[inset_0px_2px_5px_5px_rgb(0_0_0_/_0.5)] rounded-t-sm rounded-b-[12px] top-[180px] left-[130px] w-[141px] h-[103px] bg-[#72af61] flex flex-col items-center justify-center p-2 pb-1 overflow-scroll font-mono text-black text-sm">
-						<div className="absolute inset-0 grid grid-cols-[repeat(47,1fr)] grid-rows-[repeat(33,1fr)] pointer-events-none">
-							{[...Array(47 * 33)].map((_, i) => (
-								<div
-									key={`pixel-${Math.floor(i / 47)}-${i % 47}`}
-									className="border-[0.1px] border-gray-500/10"
-								/>
-							))}
-						</div>
-
-						<div className="w-full h-full flex flex-col justify-between font-mono p-0.5 space-y-0.5 overflow-hidden">
-							<div className="space-y-0.5">
-								<div className="flex items-start justify-between">
-									<div className="flex items-center gap-1">
-										<Pencil className="opacity-70 text-shadow-2xs" />
-										<p className="text-[9px] opacity-70 text-shadow-2xs">Abc</p>
-									</div>
-									<p className="text-[9px] opacity-70 text-shadow-2xs">
-										{459 - input.length}/1
-									</p>
+					<div className="w-full h-full flex flex-col justify-between space-y-0.5 overflow-hidden">
+						<div className="space-y-1">
+							<div className="flex items-start justify-between">
+								<div className="flex items-center gap-2">
+									<Pencil className="scale-150 opacity-70 text-shadow-2xs" />
+									<p className="text-sm opacity-70 text-shadow-2xs">Abc</p>
 								</div>
-								<p
-									ref={messageRef}
-									className="text-[12px] max-h-[49px] break-words opacity-70 text-shadow-2xs overflow-hidden"
-								>
-									{input || (
-										<span className="animate-pulse !duration-75">|</span>
-									)}
-									{currentKey ? (
-										<span className="animate-pulse">|</span>
-									) : (
-										<span className="opacity-0">|</span>
-									)}
+								<p className="text-sm opacity-70 text-shadow-2xs">
+									{459 - input.length}/1
 								</p>
 							</div>
-							<p className="text-[11px] opacity-70 text-shadow-2xs text-center">
-								Options
+							<p
+								ref={messageRef}
+								className="text-lg/tight max-h-[66px] break-words opacity-70 text-shadow-2xs overflow-hidden"
+							>
+								{input || <span className="animate-pulse !duration-75">|</span>}
+								{currentKey ? (
+									<span className="animate-pulse">|</span>
+								) : (
+									<span className="opacity-0">|</span>
+								)}
 							</p>
 						</div>
+						<p className="text-base opacity-70 text-shadow-2xs text-center">
+							Options
+						</p>
 					</div>
+				</div>
 
-					{/* Navigation Buttons */}
-					<div className="z-10 absolute top-[300px]">
-						<button
-							type="button"
-							onClick={handleCenterPress}
-							className="debug absolute left-[170px] w-[60px] h-[30px] rounded-full opacity-0 focus:outline-hidden"
-							aria-label="Center button"
-						/>
-						<button
-							type="button"
-							onClick={handleBackspace}
-							className="debug absolute top-[14px] left-[132px] w-[30px] h-[30px] opacity-0 focus:outline-hidden"
-							aria-label="Left button"
-						/>
-						<button
-							type="button"
-							className="debug absolute left-[220px] top-[20px] w-[50px] -rotate-12 h-[30px] opacity-0 focus:outline-hidden"
-							aria-label="Right button"
-						/>
+				{/* Navigation Buttons */}
+				<div className="z-10 absolute top-[360px] left-0">
+					<button
+						type="button"
+						onClick={handleBackspace}
+						style={{
+							top: 30,
+							left: 194,
+							width: 60,
+							height: 40,
+						}}
+						className="debug absolute rotate-45 focus:outline-hidden"
+						aria-label="Clear button"
+					/>
+					<button
+						type="button"
+						onClick={handleCenterPress}
+						style={{
+							top: 10,
+							left: 250,
+							height: 36,
+							width: 100,
+						}}
+						className="debug absolute rounded-sm focus:outline-hidden"
+						aria-label="Center button"
+					/>
+					<button
+						type="button"
+						style={{
+							top: 50,
+							width: 40,
+							left: 320,
+							height: 40,
+						}}
+						className="debug absolute -rotate-25  focus:outline-hidden"
+						aria-label="Left button"
+					/>
+					<button
+						type="button"
+						style={{
+							top: 30,
+							width: 50,
+							left: 355,
+							height: 35,
+						}}
+						className="debug absolute -rotate-25  focus:outline-hidden"
+						aria-label="Righty button"
+					/>
 
-						{/* Number Buttons */}
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="1"
-							className="debug absolute top-[64px] left-[116px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button 1"
-						/>
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="2"
-							className="debug absolute top-[69px] left-[176px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button 2"
-						/>
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="3"
-							className="debug absolute top-[64px] left-[236px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button 3"
-						/>
+					{/* Number Buttons */}
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="1"
+						style={{
+							top: 100,
+							left: 186,
+							width: 55,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button 1"
+					/>
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="2"
+						style={{
+							top: 110,
+							left: 270,
+							width: 60,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button 2"
+					/>
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="3"
+						style={{
+							top: 100,
+							left: 350,
+							width: 60,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button 3"
+					/>
 
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="4"
-							className="debug absolute top-[97px] left-[118px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button 4"
-						/>
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="5"
-							className="debug absolute top-[102px] left-[176px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button 5"
-						/>
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="6"
-							className="debug absolute top-[97px] left-[236px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button 6"
-						/>
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="4"
+						style={{
+							top: 150,
+							left: 190,
+							width: 55,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button 4"
+					/>
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="5"
+						style={{
+							top: 160,
+							left: 270,
+							width: 60,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button 5"
+					/>
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="6"
+						style={{
+							top: 150,
+							left: 350,
+							width: 60,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button 6"
+					/>
 
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="7"
-							className="debug absolute top-[129px] left-[121px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button 7"
-						/>
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="8"
-							className="debug absolute top-[135px] left-[178px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button 8"
-						/>
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="9"
-							className="debug absolute top-[131px] left-[236px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button 9"
-						/>
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="7"
+						style={{
+							top: 200,
+							left: 190,
+							width: 55,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button 7"
+					/>
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="8"
+						style={{
+							top: 210,
+							left: 270,
+							width: 60,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button 8"
+					/>
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="9"
+						style={{
+							top: 200,
+							left: 350,
+							width: 60,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button 9"
+					/>
 
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="*"
-							className="debug absolute top-[162px] left-[124px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button *"
-						/>
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="0"
-							className="debug absolute top-[169px] left-[178px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button 0"
-						/>
-						<button
-							type="button"
-							onClick={handlePress}
-							data-key="#"
-							className="debug absolute top-[162px] left-[234px] w-[49px] h-[25px] opacity-0 focus:outline-hidden touch-manipulation"
-							aria-label="Button #"
-						/>
-					</div>
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="*"
+						style={{
+							top: 250,
+							left: 195,
+							width: 55,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button *"
+					/>
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="0"
+						style={{
+							top: 260,
+							left: 270,
+							width: 60,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button 0"
+					/>
+					<button
+						type="button"
+						onClick={handlePress}
+						data-key="#"
+						style={{
+							top: 250,
+							left: 348,
+							width: 55,
+							height: 35,
+						}}
+						className="debug absolute focus:outline-hidden touch-manipulation"
+						aria-label="Button #"
+					/>
 				</div>
 			</div>
 		</div>
